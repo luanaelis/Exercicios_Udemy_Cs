50ffa6deb3bfdcef06b52cef20b0f41505c5c31d
Luana Elis <113952758+luanaelis@users.noreply.github.com>
2023-03-13 17:33:15 -0300

    Add files via upload

---
+++ ContaBancaria/Conta.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ContaBancariaClasses
+{
+    internal class Conta
+    {
+        private string _nome;
+        public int NumeroConta { get; private set; }
+        public double Saldo { get; private set; }
+
+        public Conta (int numeroConta, string nome) 
+        {
+            NumeroConta = numeroConta;
+            _nome = nome;
+        }
+
+        public Conta(int numeroConta, string nome, double saldo) : this(numeroConta, nome)
+        {
+            Saldo = saldo;
+        }
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        public double RealizarDeposito(double deposito)
+        {
+            return Saldo += deposito;
+        }
+
+        public double RealizarSaque(double saque)
+        {
+            return Saldo -= saque + 5.0;
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+                + NumeroConta
+                + ", Titular: "
+                + _nome
+                + ", Saldo: R$"
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ContaBancaria/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Runtime.Remoting.Messaging;
+
+namespace ContaBancariaClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta c;
+
+            Console.Write("Entre o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string nome = Console.ReadLine();
+            Console.Write("Haverá depósito inical? (s/n) ");
+            char resp = char.Parse(Console.ReadLine());
+            if (resp == 's' || resp == 'S')
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                c = new Conta(numero, nome, depositoInicial);
+            } else
+            {
+                c = new Conta(numero, nome);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: " + c);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para depósito: ");
+            double deposito2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.RealizarDeposito(deposito2);
+            Console.WriteLine("Dados da conta atualizados: " + c);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para saque: ");
+            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.RealizarSaque(saque);
+            Console.WriteLine("Dados da conta atualizados: " + c);
+        }
+    }
+}
